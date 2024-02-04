9f5cc6c24ff40afe5082180df15ea290a2de1333
mdoakley2 <mdo04281953@proton.me>
2024-02-04 02:38:40 -0500

    Added google chrome repo info

--- dt.bash
+++ dt.bash
@@ -5,4 +5,6 @@ printf "\nToday is $(date '+%A %B %d, %Y %H:%M:%S')"
 printf "\n============================================="
 printf "\n\n"
 cat /etc/hosts
+printf "\n\n"
+cat /etc/default/google-chrome
 exit
