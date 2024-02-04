ee89b961a6601511a657223fa874513dd7e27e13
mdoakley2 <mdo04281953@proton.me>
2024-02-04 01:48:29 -0500

    Update dt.bash for formatting

--- dt.bash
+++ dt.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 printf "\n============================================="
-printf "\nToday is $(date '+%A %B %d, %Y %H:%M:%S')\n\n"
-
+printf "\nToday is $(date '+%A %B %d, %Y %H:%M:%S')"
+printf "\n============================================="
+printf "\n"
 exit
