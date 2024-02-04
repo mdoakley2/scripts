319d51424a1c3117132fa082448d0969ae9480db
mdoakley2 <mdo04281953@proton.me>
2024-02-04 02:27:19 -0500

    Update dt.bash for displaying hosts file

--- dt.bash
+++ dt.bash
@@ -3,5 +3,6 @@
 printf "\n============================================="
 printf "\nToday is $(date '+%A %B %d, %Y %H:%M:%S')"
 printf "\n============================================="
-printf "\n"
+printf "\n\n"
+cat /etc/hosts
 exit
